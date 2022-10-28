48bc09a35dedccb2c7962f282f346836cf03e665
Eduardo-RFarias <eduardo.rfarias@outlook.com>
2022-10-28 17:20:26 -0300

    created indexes

---
+++ prisma/migrations/20221028201945_create_indexes/migration.sql
@@ -0,0 +1,14 @@
+-- CreateIndex
+CREATE INDEX "Drink_dateTime_idx" ON "Drink"("dateTime");
+
+-- CreateIndex
+CREATE INDEX "Fae_dateTime_idx" ON "Fae"("dateTime");
+
+-- CreateIndex
+CREATE INDEX "Message_createdAt_idx" ON "Message"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Post_createdAt_idx" ON "Post"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Urine_dateTime_idx" ON "Urine"("dateTime");
